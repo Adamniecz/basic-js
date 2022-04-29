9c996f657a1edb29ebb3c571b2daf81369a0a7a7
Vlad <seven.chet@gmail.com>
2022-04-29 11:17:39 +0300

    file names: delete unnecessary file

--- src/file-names.js
+++ src/file-names.js
@@ -38,25 +38,3 @@ function renameFiles(names) {
 module.exports = {
   renameFiles
 };
-
-function renameFiles1(names) {
-  let result = []
-  let fileNames = {}
-  for (let i = 0; i < names.length; i++) {
-    if (fileNames[names[i]] === undefined) {
-      if (result.includes(names[i])) {
-        fileNames[names[i]] = 1
-        result.push(names[i] + `(${fileNames[names[i]]})`)
-      } else {
-        fileNames[names[i]] = 1
-        result.push(names[i])
-      }
-    } else {
-      result.push(names[i] + `(${fileNames[names[i]]})`)
-      fileNames[names[i]]++
-    }
-  }
-  return result
-}
-
-console.log(renameFiles1(["file", "file", "image", "file(1)", "file", 'image']))
